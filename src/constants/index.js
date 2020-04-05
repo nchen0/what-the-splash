0667aa6669bf42b391cfecad872e48ad30c39c4f
nchen0 <nicky.chen0@gmail.com>
2020-04-05 11:19:33 -0500

    add stats reducer

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,4 +4,10 @@ const IMAGES = {
     LOAD_FAIL: 'IMAGES_LOAD_FAILURE',
 };
 
-export { IMAGES };
+const STATS = {
+    LOAD: 'IMAGES_LOAD',
+    LOAD_SUCCESS: 'IMAGES_LOAD_SUCCESS',
+    LOAD_FAIL: 'IMAGES_LOAD_FAILURE',
+};
+
+export { IMAGES, STATS };
