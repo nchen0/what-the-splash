ac9c5aec788e01f11f4d435acda223811c11a131
nchen0 <nicky.chen0@gmail.com>
2020-04-05 11:12:38 -0500

    images loaded

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -4,25 +4,13 @@ import { connect } from 'react-redux';
 import './styles.css';
 import { loadImages } from '../../actions';
 
-const key = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
-
 class ImageGrid extends Component {
-    state = {
-        images: [],
-    };
-
     componentDidMount() {
-        fetch(`https://api.unsplash.com/photos/?client_id=${key}&per_page=28`)
-            .then(res => res.json())
-            .then(images => {
-                this.setState({
-                    images,
-                });
-            });
+        this.props.loadImages();
     }
 
     render() {
-        const { images } = this.state;
+        const { images, error } = this.props;
         return (
             <div className="content">
                 <section className="grid">
@@ -40,6 +28,9 @@ class ImageGrid extends Component {
                         </div>
                     ))}
                     <button onClick={this.props.loadImages}>Load Images</button>
+                    {error && (
+                        <div className="error">{JSON.stringify(error)}</div>
+                    )}
                 </section>
             </div>
         );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import loadingReducer from './loadingReducer';
 import imagesReducer from './imagesReducer';
 import errorReducer from './errorReducer';
+import pageReducer from './pageReducer';
 
 export const rootReducer = combineReducers({
     isLoading: loadingReducer,
     images: imagesReducer,
     error: errorReducer,
+    nextPage: pageReducer,
 });
